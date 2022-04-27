cce9c4041f93af0a482cfc9b2de6824a2b8eac33
AntonioMiser0 <antoniomiser12@gmail.com>
2022-04-27 11:34:27 +0200

    Promjenjen je nazov MainActivitija u login, a novi main je dodan, dodan je loading screen, dizajn u mainu.

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,50 +1,27 @@
 package com.example.login;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView register, forgotPassword;
-    private EditText editTextEmail, editTextPassword;
-    private Button signIn;
-
-    private FirebaseAuth mAuth;
-
-
+    private Button login1;
+    private Button register1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        register=(TextView) findViewById(R.id.register);
-        register.setOnClickListener(this);
 
-        signIn=(Button) findViewById(R.id.signIn);
-        signIn.setOnClickListener(this);
+        login1=(Button) findViewById(R.id.login);
+        register1=(Button) findViewById(R.id.register);
+        login1.setOnClickListener(MainActivity.this);
+        register1.setOnClickListener(this);
 
-        editTextEmail=(EditText) findViewById(R.id.email);
-        editTextPassword=(EditText) findViewById(R.id.password);
-
-        mAuth=FirebaseAuth.getInstance();
-
-        forgotPassword=(TextView) findViewById(R.id.forgotPassword);
-        forgotPassword.setOnClickListener(this);
 
 
 
@@ -57,66 +34,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v){
         switch (v.getId()){
             case R.id.register:
-                startActivity(new Intent(this, RegisterUser.class));
+                startActivity(new Intent(MainActivity.this, RegisterUser.class));
                 break;
 
-            case R.id.signIn:
-                userLogin();
-                break;
-
-            case R.id.forgotPassword:
-                startActivity(new Intent(this, ForgotPassword.class));
+            case R.id.login:
+                startActivity(new Intent(MainActivity.this, Login.class));
         }
-    }
-
-    private void userLogin() {
-        String email=editTextEmail.getText().toString().trim();
-        String password=editTextPassword.getText().toString().trim();
-
-        if(email.isEmpty()){
-            editTextEmail.setError("Email is required");
-            editTextEmail.requestFocus();
-            return;
-        }
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            editTextEmail.setError("Please enter a valid email");
-            editTextEmail.requestFocus();
-            return;
-        }
-        if(password.isEmpty()){
-            editTextPassword.setError("Password is required");
-            editTextPassword.requestFocus();
-            return;
-        }
-        if (password.length()<6){
-            editTextPassword.setError("Min password lenght is 6 characters!");
-            editTextPassword.requestFocus();
-            return;
-
-        }
-
-        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-
-                    FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
-
-                    if(user.isEmailVerified()){
-                        //redirect to login page
-                        startActivity(new Intent(MainActivity.this, swp.class));
-
-
-
-                    }else{
-                        user.sendEmailVerification();
-                        Toast.makeText(MainActivity.this, "Check your email to verify your account", Toast.LENGTH_SHORT).show();
-                    }
-
-                }else{
-                    Toast.makeText(MainActivity.this, "Failed to login! Plese check your credentials", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
     }
 }
--- app/src/main/java/com/example/login/RegisterUser.java
+++ app/src/main/java/com/example/login/RegisterUser.java
@@ -46,7 +46,7 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v){
         switch (v.getId()){
             case R.id.banner:
-                startActivity(new Intent(this,MainActivity.class));
+                startActivity(new Intent(this,Login.class));
                 break;
             case R.id.registerUser:
                 registerUser();
